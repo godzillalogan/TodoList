eb962cb4aec6a47cb42c177990aec4d6d79f0c61
godzillalogan <63105483+godzillalogan@users.noreply.github.com>
2021-07-07 22:35:58 +0800

    2021.7.7,家,到S4  U87  連線資料庫並建立 Todo Model

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose') // 載入 mongoose
+
 
 const app = express()
+
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology:true}) // 設定連線到 mongoDB
+
+const db = mongoose.connection
+
+db.on('error',()=>{
+	console.log('mongodb error')
+})
+
+db.once('open',() =>{
+	console.log('mongoDB connected')
+})
+
 const port = 3000
 app.get('/',(req,res)=>{
   res.send('hello world')
