97e2339c27b9081d29ae26e40a1612a255f600fb
godzillalogan <kenlogan0117@gmail.com>
2021-07-08 17:31:07 +0800

    2021.7.8,外, S4 U89 設定 Handlebars(還沒完成)

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
+const exphbs = require('express-handlebars')
 
 const app = express()
-
+const port = 3000
 
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology:true}) // 設定連線到 mongoDB
 
@@ -16,7 +17,10 @@ db.once('open',() =>{
 	console.log('mongoDB connected')
 })
 
-const port = 3000
+app.engine('hbs',exphbs({
+	defaultLayout:'main'
+}))
+
 app.get('/',(req,res)=>{
   res.send('hello world')
 })
