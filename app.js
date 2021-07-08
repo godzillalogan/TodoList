d1a8f82af6fa7f04320023d763a231fa3b63ef6a
godzillalogan <63105483+godzillalogan@users.noreply.github.com>
2021-07-08 20:07:42 +0800

    feat: 2021.7.8,家,S4 U89 設定 Handlebars

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
-const exphbs = require('express-handlebars')
+const exphbs = require('express-handlebars') //引用express-handlebars，並命名為exphbs
 
 const app = express()
 const port = 3000
@@ -16,13 +16,19 @@ db.on('error',()=>{
 db.once('open',() =>{
 	console.log('mongoDB connected')
 })
-
+//建立一個名為hbs的樣板引擎，並傳入exphbs與相關參數
 app.engine('hbs',exphbs({
-	defaultLayout:'main'
-}))
+	defaultLayout:'main',
+	extname: '.hbs'  //是指定副檔名為.hbs，有了這行以後，我們才能把預設的長檔名改寫成短檔名。
+}))  
+
+
+//啟用樣版引擎hbs
+app.set('view engine','hbs')
+
 
 app.get('/',(req,res)=>{
-  res.send('hello world')
+  res.render('index')
 })
 
 app.listen(port,() =>{
