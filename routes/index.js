c98b7974131a1eb8a53d66a0d097cb8a7e30d116
godzillalogan <63105483+godzillalogan@users.noreply.github.com>
2021-08-04 23:28:22 +0800

    U25 驗證登入狀態

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,12 @@ const router = express.Router()
 const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/users')  
+const { authenticator } = require('../middleware/auth')  // 掛載 middleware
 
-router.use('/', home)
-router.use('/todos', todos)
+
+router.use('/todos', authenticator, todos)
 router.use('/users', users)  // add this
 
 module.exports = router
+
+router.use('/', authenticator, home)  //需要引到清單最下方，避免無限循環
