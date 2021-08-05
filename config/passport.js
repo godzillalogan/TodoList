1d2b0ba69cf8f214c878531841bdaa1fa6779d8f
godzillalogan <63105483+godzillalogan@users.noreply.github.com>
2021-08-06 03:32:26 +0800

    U31 用 bcrypt 處理密碼：登入篇

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy  //注意和一般的引用不一樣
+const bcrypt = require('bcryptjs')
 
 const User = require('../models/user')
 
@@ -12,13 +13,15 @@ module.exports = app => {
     User.findOne({ email })
     .then(user =>{
       if(!user){
-        return done(null,false,{ message:'That email is not register' })
+        return done(null, false,{ message:'That email is not register' })
       }
-      if(user.password !== password){
-        console.log("是不是密碼不對")
-        return done(null, false,{ message: 'Email or Password incorrect.' })
-      }
-      return done(null, user)
+      return bcrypt.compare(password, user.password).then(isMath =>{
+        if(!isMath){
+          console.log("是不是密碼不對")
+          return done(null, false,{ message: 'Email or Password incorrect.' })
+        }
+        return done(null, user)
+      })
     })
     .catch(err => done(err, false))
   }))
